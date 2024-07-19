0eed7fb734c447209b1ce74a0e1e79c825f958c0
Monicaselvaraj15 <143255471+Monicaselvaraj15@users.noreply.github.com>
2024-07-19 14:35:31 +0530

    Add files via upload
    C program for X pattern

---
+++ X pattern.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            if(i==j)//row equal to col
+            {
+                printf("%d",j);//print col
+             }
+            else if(i + j==n+1 || i==j)
+           {
+            printf("%d",j);
+           }
+           else
+           {
+            printf(" ");
+           }
+        }
+        printf("\n");
+    }
+    return 0;
+}
